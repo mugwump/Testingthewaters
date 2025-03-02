302f409e272a9f7a07224eb37ff00504ff5a0d0d
Stefan Frank <s.frank@vierundsechzig.de>
2025-03-02 17:31:35 +0100

    adding test for the variable args-adder

--- src/main/java/de/vierundsechzig/app/Adder.java
+++ src/main/java/de/vierundsechzig/app/Adder.java
@@ -1,5 +1,7 @@
 package de.vierundsechzig.app;
 
+import java.util.Arrays;
+
 
 public class Adder {
 
@@ -9,6 +11,28 @@ public class Adder {
 
   public static String add(String a, int b, int c) {
     return a + b + c;
+  }
+
+  public static Object varAdd(Object... varObjects) {
+    
+    Object addedUp = Arrays.asList(varObjects)
+                           .stream()
+                           .reduce(null, (a, b) -> {
+                              if (a == null) {
+                                return b;
+                              } 
+                              if(b instanceof String) {
+                                return a + (String)b;
+                              }
+                              if(a instanceof Integer) {
+                                return (Integer)a + (Integer)b;
+                              }
+                              return (String)a + b;
+
+        });
+
+    return addedUp;
+
   }
     
 }
--- src/test/java/de/vierundsechzig/app/AppTest.java
+++ src/test/java/de/vierundsechzig/app/AppTest.java
@@ -2,6 +2,8 @@ package de.vierundsechzig.app;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
 import static org.junit.Assert.assertTrue;
 
 
@@ -11,11 +13,39 @@ public class AppTest {
 
     @Test
     public void testStringCoercionWithLeadingString() {
-        assertTrue(Adder.add("1", 2, 3).equals("123"));         
+        assertEquals("Leading with a string should concatenate everything", "123", Adder.add("1", 2, 3));         
     }
 
     @Test
     public void testStringCoercionWithLeadingInt() {
-        assertTrue(Adder.add(1, 2, "3").equals("33"));
+        assertEquals("Leading numbers should be added, trailing string concatenated", Adder.add(1, 2, "3"), "33");
     }
+
+    @Test
+    public void testStringCoercionWithVarArgs() {
+        assertEquals("Summing up integer should return with the proper sum", 6, Adder.varAdd(1, 2, 3));
+    }
+
+    @Test
+    public void testStringCoercionWithVarArgsLeadingString() {
+        assertEquals("Leading string should coerce all numbers into  string", "123", Adder.varAdd("1", 2, 3));
+    }
+
+    @Test
+    public void testStringCoercionWithTrailingString() {
+        assertEquals("Leading numbers should be added up, trailing string coerced", "33", Adder.varAdd(1, 2, "3"));
+    }
+
+    @Test
+    public void testStringCoercionWithMoreVariables() {
+        assertEquals("Adding up more than 3 variables", "6FOO", Adder.varAdd(1, 2, 3, "FOO"));
+    }
+    @Test
+    public void testCallingTheAdderWithNonIntegerString() {
+        int[] numbers = {1, 2, 3};
+        assertThrows(ClassCastException.class, () -> Adder.varAdd(1, 2, numbers, "FOO"));
+    }
+
+
+
 }
