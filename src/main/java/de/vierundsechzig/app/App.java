563fda8bd8ad7da208719b970c3de659b8142d30
Stefan Frank <s.frank@vierundsechzig.de>
2025-03-02 16:12:53 +0100

    fix: initial tests for the straighforward adder

--- src/main/java/de/vierundsechzig/app/App.java
+++ src/main/java/de/vierundsechzig/app/App.java
@@ -1,12 +1,22 @@
 /*----------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Copyright (c) vierundsechzig.de. All rights reserved.
  * Licensed under the MIT License. See LICENSE in the project root for license information.
  *---------------------------------------------------------------------------------------*/
 
 package de.vierundsechzig.app;
 
 public class App {
+
+    private static int v=1;
+    
+    public static void doStuff(int x) {
+        v++; 
+        x+=v;
+    }
+    
     public static void main(String[] args) {
-        System.out.println("Hello Remote World!");
+        int num = 10;
+        doStuff(num);
+        System.out.println("Result is " + num);
     }
 }
